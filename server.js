c030dfcf0599781b894e2bc19f186377363b5597
Tim Hwang <Tim@Tims-MBP.lan>
2016-09-27 22:25:18 -0700

    commiting changes to server.js to make html page appear

--- server.js
+++ server.js
@@ -10,13 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/app/public/home.html'));
-});
-
-app.get('/survey', function (req, res) {
-    res.sendFile(path.join(__dirname, '/app/public/survey.html'));
-});
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
 
 app.listen(PORT, function () {
     console.log('App listening on PORT ' + PORT);
